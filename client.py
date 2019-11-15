7238828e9dc598ea49b8d4fc19b9706b6567c2f0
Alisa Hussain <alisa.hussain@hotmail.com>
2019-11-15 15:35:02 +0000

    presents options to user + handles QUIT

--- client.py
+++ client.py
@@ -20,17 +20,42 @@ serverPort = int(sys.argv[2]);
 clientSocket = socket(AF_INET, SOCK_STREAM);
 clientSocket.connect((serverIP, serverPort));
 try:
-    clientSocket.send('GET_BOARDS'.encode());
-    boardList = clientSocket.recv(1024);
-    if pickle.loads(boardList) == 100:
-        printError(100);
-    elif pickle.loads(boardList) == 101:
-        printError(101);
-    else:
-        fBoardList = formatBoardList(boardList);
-        print('Message boards: ', fBoardList);
+    while True:
+        getUserOptions = True;
+        clientSocket.send('GET_BOARDS'.encode());
+        boardList = clientSocket.recv(1024);
+        if pickle.loads(boardList) == 100:
+            printError(100);
+            getUserOptions = False;
+        elif pickle.loads(boardList) == 101:
+            printError(101);
+            getUserOptions = False;
+            break;
+        else:
+            fBoardList = formatBoardList(boardList);
+            print('Boards retrieved successfully! \n Message boards: ', fBoardList);
+        while True and getUserOptions == True:
+            inputError = True;
+            while inputError == True:
+                inputError = False;
+                userSelect = input('Input: \n - A board number \n - POST to post a message \n - QUIT \n');
+                if userSelect == "QUIT":
+                    print("quit");
+                    clientSocket.close();
+                    sys.exit();
+                elif userSelect == "POST":
+                    print("post");
+                elif userSelect in range (1, (len(fBoardList)+1)):
+                    print("board number selected");
+                else:
+                    print("ERROR: Invalid input, try again")
+                    inputError == True;
+
+
+
 finally:
     clientSocket.close();
+    sys.exit();
 
 
 
--- server.py
+++ server.py
@@ -20,28 +20,27 @@ except error as e:
 if portConnected:
     serverSocket.listen(1);
     print('Server is ready to receive');
-
+    connectionSocket, addr = serverSocket.accept();
+    print("Connection from: " + str(addr));
     while True:
-        connectionSocket, addr = serverSocket.accept();
         recvMessage = connectionSocket.recv(1024).decode();
-
-        try:
-            if recvMessage:
-                if recvMessage == "GET_BOARDS":
-                    boardList = os.listdir("./board");
-                    for item in boardList:
-                        if item[0] == '.':
-                            boardList.remove(item);
-                    if len(boardList) == 0:
-                        print("ERROR: No message boards defined");
-                        connectionSocket.send(pickle.dumps(101));
-                        serverSocket.close()
-                        break
-                    boardListToSend = pickle.dumps(boardList);
-                    print(boardList);
-                    connectionSocket.send(boardListToSend);
-                else:
-                    sendMessage = 100;
-                    connectionSocket.send(pickle.dumps(sendMessage));
-        finally:
-            connectionSocket.close();
+        if recvMessage:
+            if recvMessage == "GET_BOARDS":
+                boardList = os.listdir("./board");
+                for item in boardList:
+                    if item[0] == '.':
+                        boardList.remove(item);
+                if len(boardList) == 0:
+                    print("ERROR: No message boards defined");
+                    connectionSocket.send(pickle.dumps(101));
+                    serverSocket.close()
+                    break;
+                boardListToSend = pickle.dumps(boardList);
+                print(boardList);
+                connectionSocket.send(boardListToSend);
+            else:
+                sendMessage = 100;
+                connectionSocket.send(pickle.dumps(sendMessage));
+        else:
+            break;
+    connectionSocket.close();
