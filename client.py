7bfec2e3a3f061adcd12e708c451a7cbb598f531
Alisa Hussain <alisa.hussain@hotmail.com>
2019-11-20 14:35:49 +0000

    all main methods created

--- client.py
+++ client.py
@@ -14,48 +14,72 @@ def printError(errorVal):
     if errorVal == 101:
         print("No message boards have been defined");
 
+def getMessages(boardIndex, boardList):
+    boardName = boardList[boardIndex].split()[1];
+    clientSocket.send(('GET_MESSAGES('+boardName+')').encode());
+    rtnMsg = pickle.loads(clientSocket.recv(1024));
+    return rtnMsg;
+
+def postMessage(numBoards):
+    boardNumError = True;
+    while boardNumError == True:
+        boardNum = int(input("Select the number of the board to write to: \n"));
+        if (boardNum > 0) and (boardNum <= numBoards):
+            boardNumError = False;
+    msgName = input("Title of your message: \n")
+    msgName = msgName.replace(' ', '_');
+    msgContents = input("Content of your message: \n");
+    sendData = 'POST_MESSAGE' + '\n' + str(boardNum) + '\n' + msgName + '\n' + msgContents;
+    clientSocket.send(sendData.encode());
+    status = pickle.loads(clientSocket.recv(1024));
+    return status;
+
 
 serverIP = (sys.argv[1]);
 serverPort = int(sys.argv[2]);
 clientSocket = socket(AF_INET, SOCK_STREAM);
 clientSocket.connect((serverIP, serverPort));
-try:
-    while True:
-        getUserOptions = True;
-        clientSocket.send('GET_BOARDS'.encode());
-        boardList = clientSocket.recv(1024);
-        if pickle.loads(boardList) == 100:
-            printError(100);
-            getUserOptions = False;
-        elif pickle.loads(boardList) == 101:
-            printError(101);
-            getUserOptions = False;
-            break;
-        else:
-            fBoardList = formatBoardList(boardList);
-            print('Boards retrieved successfully! \n Message boards: ', fBoardList);
-        while True and getUserOptions == True:
-            inputError = True;
-            while inputError == True:
-                inputError = False;
-                userSelect = input('Input: \n - A board number \n - POST to post a message \n - QUIT \n');
-                if userSelect == "QUIT":
-                    print("quit");
-                    clientSocket.close();
-                    sys.exit();
-                elif userSelect == "POST":
-                    print("post");
-                elif userSelect in range (1, (len(fBoardList)+1)):
-                    print("board number selected");
+while True:
+    getUserOptions = True;
+    clientSocket.send('GET_BOARDS'.encode());
+    boardList = clientSocket.recv(1024);
+    if pickle.loads(boardList) == 100:
+        printError(100);
+        getUserOptions = False;
+    elif pickle.loads(boardList) == 101:
+        printError(101);
+        getUserOptions = False;
+        break;
+    else:
+        fBoardList = formatBoardList(boardList);
+        print('Boards retrieved successfully! \n Message boards: ', fBoardList);
+    while getUserOptions == True:  # instead of while True
+        inputError = True;
+        while inputError == True:
+            inputError = False;
+            userSelect = input('Input: \n - A board number \n - POST to post a message \n - QUIT \n');
+            if userSelect == "QUIT":
+                print("quit");
+                clientSocket.close();
+                sys.exit();
+            elif userSelect == "POST":
+                returnData = postMessage(len(fBoardList));
+                print(returnData);
+            elif int(userSelect) in range(1, (len(fBoardList) + 1)):
+                print("board number selected");
+                displayMsg = getMessages((int(userSelect) - 1), fBoardList);
+                if displayMsg == []:
+                    print('No messages yet in this board');
                 else:
-                    print("ERROR: Invalid input, try again")
-                    inputError == True;
+                    print('Latest Messages From This Board: ', displayMsg);
+            else:
+                print("ERROR: Invalid input, try again")
+                inputError == True;
 
 
 
-finally:
-    clientSocket.close();
-    sys.exit();
+clientSocket.close();
+sys.exit();
 
 
 
--- server.py
+++ server.py
@@ -1,8 +1,41 @@
 from socket import *
-import errno
-import sys
-import os
-import pickle
+import glob;
+import sys;
+import os;
+import pickle;
+import datetime;
+
+def getMessages(aBoardName):
+    thePath = os.getcwd() + '/board/'+ aBoardName +'/*';
+    print(thePath);
+    allFiles = glob.glob(thePath);
+    sorted_files = sorted(allFiles, key=os.path.getctime);
+    rtnMessages = [];
+    i = 1;
+    while i < 101 and i < len(sorted_files) + 1:
+        with open(sorted_files[(i*-1)], "r") as aFile:
+            data = aFile.read().replace('\n', ' ');
+            aFile.close();
+        rtnMessages.append(data);
+        i += 1;
+    return rtnMessages;
+
+def postMessage(boardList, boardNum, msgTitle, msgContents):
+    currentDT = (str(datetime.datetime.now())[:19]).replace(' ', '-').replace(':','');
+    msgTitle = currentDT + '-' + msgTitle;
+    aBoardName = boardList[int(boardNum)-1];
+    thePath = os.getcwd() + '/board/' + aBoardName;
+    completeFileName = os.path.join(thePath, msgTitle + '.txt');
+    print('path ', completeFileName);
+    try:
+        aFile = open(completeFileName, 'w+');
+        aFile.write(msgContents);
+        aFile.close();
+        return "success";
+    except:
+        print('an error has occurred');
+        return "error";
+
 serverIP = (sys.argv[1]);
 serverPort = int(sys.argv[2]);
 serverSocket = socket(AF_INET, SOCK_STREAM);
@@ -36,8 +69,16 @@ if portConnected:
                     serverSocket.close()
                     break;
                 boardListToSend = pickle.dumps(boardList);
-                print(boardList);
                 connectionSocket.send(boardListToSend);
+            elif recvMessage[0:13] == "GET_MESSAGES(":
+                boardName = recvMessage[13:len(recvMessage)-1];
+                rtnMessages = getMessages(boardName);
+                connectionSocket.send(pickle.dumps(rtnMessages));
+
+            elif recvMessage.split('\n')[0] == "POST_MESSAGE":
+                parameters = recvMessage.split('\n');
+                status = postMessage(boardList, parameters[1], parameters[2], parameters[3]);
+                connectionSocket.send(pickle.dumps(status));
             else:
                 sendMessage = 100;
                 connectionSocket.send(pickle.dumps(sendMessage));
